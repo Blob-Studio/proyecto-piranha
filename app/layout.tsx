009e228e81dfcecb08e56ed15953a796f782f25e
Guido La Rosa <guido.glarosa@gmail.com>
2023-09-30 21:25:11 -0300

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,41 @@ const ibmPlexMono = IBM_Plex_Mono({
 
 export const metadata: Metadata = {
   title: "Proyecto Piranha",
-  description: "Proyecto Piranha",
+  description: "Proyecto Piranha es una editorial y productora cultural.",
+  authors: [{ name: "Guido La Rosa", url: "https://blob.studio" }],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+    },
+  },
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Proyecto Piranha",
+    description: "Proyecto Piranha es una editorial y productora cultural.",
+    images: ["https://i.ibb.co/99F3wgr/pianhapiranha.png"],
+  },
+  openGraph: {
+    title: "Proyecto Piranha",
+    url: "https://proyectopiranha.com",
+    type: "website",
+    description: "Proyecto Piranha es una editorial y productora cultural.",
+    images: [
+      {
+        url: "https://i.ibb.co/99F3wgr/pianhapiranha.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
