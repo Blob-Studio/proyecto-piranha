fce8c83793a2f83f2f2d557b8b7e808542b1b18b
Guido La Rosa <guido.glarosa@gmail.com>
2023-10-01 09:34:26 -0300

    Updated styles

--- app/components/common/Root.tsx
+++ app/components/common/Root.tsx
@@ -5,14 +5,14 @@ import Marquee from "react-fast-marquee";
 
 const Root = (props: any) => {
   return (
-    <div className="w-full flex h-[100dvh]">
-      <aside className="h-full w-2/12 z-10 bg-black">
+    <div className="flex h-[100dvh]">
+      <aside className="h-full w-[260px] z-10 bg-black">
         <Navbar />
       </aside>
-      <div className="max-h-[100dvh] overflow-y-scroll w-10/12">
+      <div className="w-[calc(100%-260px)] max-h-[100dvh] overflow-y-scroll">
         <main>
-          <div className="top-bar border-b h-20 bg-green-500 text-5xl flex items-center font-semibold">
-            <Marquee>¡NUEVO PRODUCTO! BUZO DE VICKY MONTE → → → </Marquee>
+          <div className="top-bar border-b h-20 bg-amber-700 text-green-500 text-5xl flex items-center font-semibold">
+            <Marquee className="h-full">¡NUEVO PRODUCTO! BUZO DE VICKY MONTE → → → </Marquee>
           </div>
           {props.children}
         </main>
